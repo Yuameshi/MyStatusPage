0ec30148b0193581334cbdef740d44c38d81367a
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-06-06 16:15:29 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+window.Config = {
+	SiteName: 'Yuameshiの服务状态',
+	SiteUrl: '/',
+	ApiKeys: [
+		'm788169884-88ef582a033dc0a6c5166a25', //Blog
+		'm788169970-b20a51146f1a6a9a7f789e2c', //AyaneYe
+		'm788171336-1fa5e9ca257602dcf12a92ba', //CDN
+		'm788917394-0c2b544960d43e59255f43ff', //DBS Mirror
+		'm788169976-1aadb9d81fe758b88f7f7d18', //Blog Old
+	],
+	ShowLink: true,
+	CountDays: 60,
+	Navi: [
+		{
+			text: 'Yuameshiの窝',
+			url: 'https://www.yuameshi.top'
+		},
+		{
+			text: 'GitHub',
+			url: 'https://github.com/Yuameshi/'
+		}
+	]
+};
